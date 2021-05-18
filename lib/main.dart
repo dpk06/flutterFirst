1f0e13e9d0063f5df033ea36385068621e3c8fc6
Deepak yadav <deepak.yadav@perennialsys.com>
2021-05-18 16:17:28 +0530

    Chap 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,35 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awsome app"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi fluter")),
-      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+                BoxShadow(
+                color: Colors.grey[400],
+                blurRadius: 5,
+                offset: Offset(2.0, 10.0 )
+              )
+            ],
+            color: Colors.teal,
+            gradient: LinearGradient(colors: [Colors.yellow, Colors.pink ])
+          ),
+          child: Text(
+              "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+               fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      )
     );
   }
 }
