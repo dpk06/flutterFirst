cbc994467d42f1c3dbe999631a1eadb3c1c615bb
Deepak yadav <deepak.yadav@perennialsys.com>
2021-05-18 16:40:52 +0530

    Chap 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Awsome app",
-    home: Homepage()
+      title: "Awsome app",
+      home: Homepage()
   ));
 }
 
@@ -14,38 +14,47 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awsome app"),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-                BoxShadow(
-                color: Colors.grey[400],
-                blurRadius: 5,
-                offset: Offset(2.0, 10.0 )
-              )
-            ],
-            color: Colors.teal,
-            gradient: LinearGradient(colors: [Colors.yellow, Colors.pink ])
-          ),
-          child: Text(
-              "I am a box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-               fontWeight: FontWeight.bold,
-              fontSize: 20,
+        appBar: AppBar(
+          title: Text("Awsome app"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 400,
+              height: 500,
+              color: Colors.yellow,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue ,
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-      )
+        )
     );
   }
 }
